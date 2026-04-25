f431d27a7454366a73502f04fbe84b3d8832ecaf
Ailten <faouzi26b@live.fr>
2026-04-25 10:03:21 +0200

    Simple Patern

---
+++ 16_SimplePatern.py
@@ -0,0 +1,19 @@
+import re
+
+# Simple Patern.
+# (?)
+
+# take a string, return true if eatch letter alphabetic has a char '+' before and after.
+
+
+
+def simplePatern(line):
+
+    return re.search('([^+][a-z]|[a-z][^+])', line) == None
+
+
+
+print(simplePatern('abc'))
+print(simplePatern('+a+b+++c+'))
+print(simplePatern('$*-_+a+__-'))
+print(simplePatern('-a-'))
