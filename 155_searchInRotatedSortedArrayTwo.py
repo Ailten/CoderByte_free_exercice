1185ac07b913b3fb2fe03453aa3479c220b4c0bb
Ailten <faouzi26b@live.fr>
2026-07-24 13:20:10 +0200

    search rotate sorted

---
+++ 155_searchInRotatedSortedArrayTwo.py
@@ -0,0 +1,39 @@
+
+# search in rotated sorted array two.
+# https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+
+def func(nums: list[int], target: int) -> bool:
+
+    if len(nums) == 0:
+        return False
+
+    if nums[0] == target:
+        return True
+    elif len(nums) == 1:
+        return False
+    
+    is_increment = nums[0] < target
+
+    a = 0
+    b = len(nums) // 2
+
+    if is_increment and nums[b] > target:
+        return func(nums[a:b], target)
+    else:
+        return func(nums[b:], target)
+
+
+print(func([2,5,6,0,0,1,2], 0))  # True.
+print(func([2,5,6,0,0,1,2], 3))  # False.
+
+print(func([], 0))
+print(func([1], 0))
+print(func([1,1], 0))
+print(func([1,0,1], 0))
+print(func([0,1,1], 0))
+print(func([1,1,0], 0))
+print(func([1,0,1,1], 0))  # not working in this case, FIXME.
+print(func([0,1,1,1], 0))
+print(func([1,1,0,1], 0))
+print(func([1,1,1,0], 0))
