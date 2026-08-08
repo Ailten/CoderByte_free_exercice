0469b4df2343ff309b35e416ac5dcb46640a19fb
Ailten <faouzi26b@live.fr>
2026-08-08 13:12:52 +0200

    optimisation

--- 172_UniqueBinarySearchTrees.py
+++ 172_UniqueBinarySearchTrees.py
@@ -21,3 +21,28 @@ def numTrees(n: int) -> int:
 print(numTrees(1))  # 1.
 print(numTrees(2))  # 2.
 print(numTrees(3))  # 5.
+print(numTrees(4))  # 30.
+
+
+# ---> v2.
+
+# without recursivity, more opti (keep last result in memory).
+def numTrees_v2(n: int) -> int:
+
+    if n <= 0:
+        return 0
+    if n <= 2:
+        return n
+
+    last_ans = 2
+    pow_sum = 1
+    for i in range(3, n+1):
+        pow_sum += pow(last_ans, 2)
+        last_ans = pow_sum
+    return last_ans
+
+
+print(' --- v2 ---')
+print(numTrees_v2(3))  # 5.
+print(numTrees_v2(4))  # 30.
+# 15 is max integer string pyhton allow (over, need to use a dedicate library).
