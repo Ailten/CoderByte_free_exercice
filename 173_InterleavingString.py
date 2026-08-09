f5a603e99e747d1455ce5a804b738a2e2d30fbb4
Ailten <faouzi26b@live.fr>
2026-08-09 17:14:58 +0200

    173 back track

--- 173_InterleavingString.py
+++ 173_InterleavingString.py
@@ -44,3 +44,85 @@ print(isInterleave("aab", "c", "caab"))  # True.
 print(isInterleave("aab", "c", "z"))  # False.
 print(isInterleave("aab", "c", "bcaa"))  # False.
 print(isInterleave("aabcc", "dbbca", "aadbbcbcac"))  # True.
+
+
+# ---> v2.
+
+# with back tracking.
+def isInterleave_v2(s1: str, s2: str, s3: str) -> bool:
+
+    if len(s1) + len(s2) != len(s3):
+        return False
+
+    i1 = 0
+    i2 = 0
+    i3 = 0
+    is_back_track = False
+    actions: list[tuple] = []
+    while True:
+        if i3 == 0 and is_back_track:
+            return False
+        if i1 == len(s1):
+            if s3[i3:] == s2[i2:]:
+                return True
+            is_back_track = True
+        if i2 == len(s2):
+            if s3[i3:] == s1[i1:]:
+                return True
+            is_back_track = True
+
+        if is_back_track:
+            last_action = actions.pop()
+
+            if last_action[0] == 1:
+                i1 -= 1
+            else:
+                i2 -= 1
+            
+            if not last_action[1]:
+                i3 -= 1
+                continue
+
+            if last_action[0] == 1:
+                actions.append((2, False))
+                i2 += 1
+            else:
+                actions.append((1, False))
+                i1 += 1
+            is_back_track = False
+            continue
+
+
+        c1 = s1[i1]
+        c2 = s2[i2]
+        c3 = s3[i3]
+        is_s1_match = c1 == c3
+        is_s2_match = c2 == c3
+
+        if is_s1_match ^ is_s2_match:
+            if is_s1_match:
+                actions.append((1, False))
+                i1 += 1
+            else:
+                actions.append((2, False))
+                i2 += 1
+            i3 += 1
+            continue
+        
+        if not is_s1_match and not is_s2_match:
+            is_back_track = True
+            continue
+
+        # when have both valid, thake the s1, (and not both is valid for back track.)
+        actions.append((1, True, c1))
+        i1 += 1
+        i3 += 1
+
+
+print(' --- V2 ---')
+print(isInterleave_v2("aab", "c", "aabc"))  # True.
+print(isInterleave_v2("aab", "c", "acab"))  # True.
+print(isInterleave_v2("aab", "c", "caab"))  # True.
+print(isInterleave_v2("aab", "c", "z"))  # False.
+print(isInterleave_v2("aab", "c", "bcaa"))  # False.
+print(isInterleave_v2("aabcc", "dbbca", "aadbbcbcac"))  # True.
